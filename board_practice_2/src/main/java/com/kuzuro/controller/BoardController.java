612ef4c6724debdd43e995a389f2a019856e0c09
jaekk <awbwd4@gmail.com>
2019-03-05 17:17:27 +0900

    Controller까지 구현

--- board_practice_2/src/main/java/com/kuzuro/controller/BoardController.java
+++ board_practice_2/src/main/java/com/kuzuro/controller/BoardController.java
@@ -3,8 +3,12 @@ package com.kuzuro.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import com.kuzuro.domain.BoardVO;
 import com.kuzuro.service.BoardService;
 
 import lombok.AllArgsConstructor;
@@ -27,6 +31,36 @@ public class BoardController {
 		
 	}
 	
+	@GetMapping("/get")
+	public void get(@RequestParam("bno") int bno, Model model) {
+		
+		log.info("/get");
+		model.addAttribute("board", service.get(bno));
+		
+	}
+	
+	@PostMapping("/modify")
+	public String modify(BoardVO board, RedirectAttributes rttr) {
+		
+		log.info("modifiy:"+board);
+		
+		if(service.modify(board)) {
+			rttr.addFlashAttribute("result", "success");
+		}
+			
+		return "redirect:/board/list";
+	}
+	
+	@PostMapping("/remove")
+	public String remove(@RequestParam("bno") int bno, RedirectAttributes rttr) {
+		
+		log.info(bno+"remove..................................................................................................");
+		
+		if(service.remove(bno)){
+			rttr.addFlashAttribute("result", "success");
+		}
+		return "redirect:/board/list";
+	}
 	
 	
 	
--- board_practice_2/src/test/java/com/kuzuro/controller/BoardControllerTests.java
+++ board_practice_2/src/test/java/com/kuzuro/controller/BoardControllerTests.java
@@ -43,7 +43,45 @@ public class BoardControllerTests {
 				.getModelMap());
 	}
 	
+	@Test
+	public void testGet() throws Exception{
+		
+		log.info(mockMvc.perform(MockMvcRequestBuilders
+			.get("/board/get")
+			.param("bno", "2"))
+				.andReturn()
+				.getModelAndView().getModelMap());
+	}
 	
+	
+	@Test
+	public void testModify() throws Exception{
+		
+		String resultPage = mockMvc
+				.perform(MockMvcRequestBuilders.post("/board/modify")
+						.param("bno", "1")
+						.param("title", "수정된 테스트 새글 제목")
+						.param("content", "수정된 테스트 새글 내용")
+						.param("writer","user00"))
+					.andReturn().getModelAndView().getViewName();
+			
+		log.info(resultPage);
+		
+	}
+	
+	@Test
+	public void testRemove() throws Exception{
+		//삭제 전 데이터베이스에서 게시물 번호 확인할 것
+		
+		String resultPage = mockMvc
+				.perform(MockMvcRequestBuilders.post("/board/remove")
+						.param("bno", "13"))
+					.andReturn().getModelAndView().getViewName();
+		
+		log.info(resultPage);
+		
+		
+	}
 	
 	
 	
