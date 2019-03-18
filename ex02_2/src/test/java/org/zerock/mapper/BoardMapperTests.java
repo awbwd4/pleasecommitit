2dfe1f02c75f574cb3147b32c63570de9d6a269d
jaekk <awbwd4@gmail.com>
2019-03-18 16:19:45 +0900

    ch.13 - MyBatis와 스프링에서 페이지 처리

--- ex02_2/src/test/java/org/zerock/mapper/BoardMapperTests.java
+++ ex02_2/src/test/java/org/zerock/mapper/BoardMapperTests.java
@@ -1,11 +1,14 @@
 package org.zerock.mapper;
 
+import java.util.List;
+
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.zerock.domain.BoardVO;
+import org.zerock.domain.Criteria;
 
 import lombok.Setter;
 import lombok.extern.log4j.Log4j;
@@ -18,11 +21,12 @@ public class BoardMapperTests {
 	@Setter(onMethod_= @Autowired)
 	private BoardMapper mapper;
 	
+	/*
+	
 	@Test
 	public void testGetList() {
 		mapper.getList().forEach(board -> log.info(board));
 	}
-	
 	@Test
 	public void testInsert() {
 		
@@ -88,6 +92,23 @@ public class BoardMapperTests {
 		log.info("UPDATE COUNT: "+count);
 	}
 	
+	
+	
+	 */
+	
+	@Test
+	public void testPaging() {
+		
+		Criteria cri = new Criteria();
+		cri.setPageNum(1);
+		cri.setAmount(10);
+		
+		List<BoardVO> list = mapper.getListWithPaging(cri);
+		
+		list.forEach(board -> log.info(board));
+		
+		
+	}
 	
 	
 	
--- ex02_2/src/test/java/org/zerock/service/BoardServiceTests.java
+++ ex02_2/src/test/java/org/zerock/service/BoardServiceTests.java
@@ -8,6 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.zerock.domain.BoardVO;
+import org.zerock.domain.Criteria;
 
 import lombok.Setter;
 import lombok.extern.log4j.Log4j;
@@ -20,7 +21,7 @@ public class BoardServiceTests {
 
 	@Setter(onMethod_ = @Autowired)
 	private BoardService service;
-	
+	/*
 	@Test
 	public void testExist() {
 		log.info(service);
@@ -43,14 +44,18 @@ public class BoardServiceTests {
 		log.info("생성된 게시물의 번호 : "+board.getBno());
 		
 	}
-	
+	*/
 	@Test
 	public void testGetList() {
 		
-		service.getList().forEach(board -> log.info(board));
+		//service.getList().forEach(board -> log.info(board));
+		service.getList(new Criteria(2, 10)).forEach(board -> log.info(board));
 	}
 	
+	/*
+	
 	@Test
+	
 	public void testGet() {
 		
 		log.info(service.get(12L));
@@ -80,6 +85,7 @@ public class BoardServiceTests {
 		
 	}
 	
+	 */
 	
 	
 }
