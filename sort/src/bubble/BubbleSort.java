a485ce4b354dd962b0bedd2f7057789ee5243d6e
jaekk <awbwd4@gmail.com>
2019-05-10 20:08:23 +0900

    정렬연습 : 버블정렬

--- sort/src/bubble/BubbleSort.java
+++ sort/src/bubble/BubbleSort.java
@@ -2,6 +2,44 @@ package bubble;
 
 public class BubbleSort {
 	
+	public static void main(String[] args) {
+		
+		int a[] = {69, 10, 30, 2, 16, 8, 31, 22};
+		
+		int temp = 0;
+		
+		bubbleSort(a);
+		
+		for (int i = 0; i < a.length; i++) {
+			System.out.print(a[i]+"//");
+		}
+		
+	}
+	
+	
+	public static void bubbleSort(int[] a) {
+		
+		for (int j = 0; j < a.length; j++) {
+			
+			for (int i = 0; i < a.length-1; i++) {
+				
+				if (a[i] > a[i+1]) {
+					
+					swap(a, i, j);
+				}
+			}
+		}
+		
+	}
+	
+	
+	public static void swap(int[] a, int i, int j) {
+		
+		int temp = a[i];
+		a[i] = a[i+1];
+		a[i+1] = temp;
+		
+	}
 	
 	
 }
